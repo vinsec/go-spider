0bc55c41d1997c3e695a3b0db7fb2989fd032cc5
vinsec <vinsec@users.noreply.github.com>
2024-02-08 21:53:07 +0800

    fix directory struct

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,9 @@ package config
 
 import (
 	"errors"
+	"os"
 	"path/filepath"
+	"strconv"
 )
 
 import (
@@ -54,6 +56,15 @@ func LoadConfigFromFile(filePath string) (*Config, error) {
 		return nil, err
 	}
 
+	// Override config values with environment variables if they exist
+	conf.Spider.UrlListFile = getEnv("SPIDER_URL_LIST_FILE", conf.Spider.UrlListFile)
+	conf.Spider.OutputDirectory = getEnv("SPIDER_OUTPUT_DIRECTORY", conf.Spider.OutputDirectory)
+	conf.Spider.MaxDepth = getEnvAsInt("SPIDER_MAX_DEPTH", conf.Spider.MaxDepth)
+	conf.Spider.CrawlInterval = getEnvAsInt("SPIDER_CRAWL_INTERVAL", conf.Spider.CrawlInterval)
+	conf.Spider.CrawlTimeout = getEnvAsInt("SPIDER_CRAWL_TIMEOUT", conf.Spider.CrawlTimeout)
+	conf.Spider.TargetUrl = getEnv("SPIDER_TARGET_URL", conf.Spider.TargetUrl)
+	conf.Spider.ThreadCount = getEnvAsInt("SPIDER_THREAD_COUNT", conf.Spider.ThreadCount)
+
 	configDir := filepath.Dir(filePath)
 	conf.Spider.UrlListFile = resolvePath(configDir, conf.Spider.UrlListFile)
 	conf.Spider.OutputDirectory = resolvePath(configDir, conf.Spider.OutputDirectory)
@@ -70,4 +81,27 @@ func resolvePath(basePath, targetPath string) string {
 		return targetPath
 	}
 	return filepath.Join(basePath, targetPath)
+}
+
+// getEnv retrieves the value of the environment variable named by the key.
+// If the variable is present in the environment the function returns its value.
+// If the variable is not present, it returns the default value.
+func getEnv(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
+}
+
+// getEnvAsInt is a helper function that retrieves the value of the environment variable as an integer.
+func getEnvAsInt(name string, defaultValue int) int {
+	valueStr := getEnv(name, "")
+	if valueStr == "" {
+		return defaultValue
+	}
+	value, err := strconv.Atoi(valueStr)
+	if err != nil {
+		return defaultValue
+	}
+	return value
 }
--- internal/go.mod
+++ internal/go.mod
@@ -1,12 +1,16 @@
 module github.com/vinsec/go-spider
 
-go 1.16
+go 1.21
 
 require (
 	github.com/go-gcfg/gcfg v1.2.3
 	github.com/jeanphorn/log4go v0.0.0-20190526082429-7dbb8deb9468
+	golang.org/x/net v0.21.0
+)
+
+require (
 	github.com/toolkits/file v0.0.0-20160325033739-a5b3c5147e07 // indirect
-	golang.org/x/net v0.0.0-20220826154423-83b083e8dc8b
+	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/gcfg.v1 v1.2.3 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 )
